9a5f05cab0cf37c180a8ee41df7b4967a0ab0665
Kenan Sulayman <kenan@sly.mn>
2018-08-26 20:25:38 +0200

    Fix markup, add metrics

--- main.js
+++ main.js
@@ -1,4 +1,75 @@
+const imageLoader = () => {
+    let lazyImages = [].slice.call(document.querySelectorAll("img"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    if (entry.target.dataset.src) {
+                        entry.target.src = entry.target.dataset.src;
+                    }
+
+                    if (entry.target.dataset.srcset) {
+                        entry.target.srcset = entry.target.dataset.srcset;
+                    }
+
+                    lazyImageObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyImages.forEach(lazyImage => {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        let active = false;
+
+        const checkImages = () => {
+            lazyImages.forEach(lazyImage => {
+                if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+                    if (lazyImage.dataset.src) {
+                        lazyImage.src = lazyImage.dataset.src;
+                    }
+                    if (lazyImage.dataset.srcset) {
+                        lazyImage.srcset = lazyImage.dataset.srcset;
+                    }
+
+                    lazyImages = lazyImages.filter(image =>
+                        (image !== lazyImage)
+                    );
+
+                    if (lazyImages.length === 0) {
+                        document.removeEventListener("scroll", lazyLoad);
+                        window.removeEventListener("resize", lazyLoad);
+                        window.removeEventListener("orientationchange", lazyLoad);
+                    }
+                }
+            });
+
+            active = false;
+        };
+
+        const lazyLoad = () => {
+            if (active === false) {
+                active = true;
+
+                setTimeout(() => {
+                    checkImages();
+                }, 200);
+            }
+        };
+
+        checkImages();
+
+        document.addEventListener("scroll", lazyLoad);
+        window.addEventListener("resize", lazyLoad);
+        window.addEventListener("orientationchange", lazyLoad);
+    }
+};
+
 const init = () => {
+    imageLoader();
+
     const toggle = document.querySelector('.navbar-toggle');
     const navmenu = document.querySelector('.navigation');
 
@@ -36,5 +107,5 @@ const init = () => {
 if (document.readyState === 'complete') {
     init();
 } else {
-    window.addEventListener('load', init);
+    window.addEventListener('DOMContentLoaded', init);
 }
